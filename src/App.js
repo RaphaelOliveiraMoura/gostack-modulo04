29ab9280877cd0662fa5d14aff3645ba5466d30b
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-26 14:03:01 -0300

    feat: Import css and image files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import './App.css';
+import profile from './assets/profile.jpeg';
+
 export default function src() {
-  return <div>Oi</div>;
+  return (
+    <div>
+      <img src={profile} alt="Profile" />
+    </div>
+  );
 }
