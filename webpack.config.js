1e8fd91a84856a574407a387052db1b59d0b7a6f
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-26 14:02:45 -0300

    chore: Add style and files loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /.*\.(gif|jpe?g|png)$/i,
+        use: { loader: 'file-loader' }
       }
     ]
   }
